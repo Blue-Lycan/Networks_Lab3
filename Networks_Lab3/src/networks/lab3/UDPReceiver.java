a30812630e1d4b78c6568a71be500cbe5f55cc47
TJSchwartzt <TJSchwartzt@gmail.com>
2015-10-04 16:38:29 -0600

    Created Several methods for Receiver Class, descriptions in message body
    Methods include: Receive data method, initialize window method, print
    window, and shift window to the right methods. I commented out receive
    data method in case you have a different receive data method you've been
    using.

--- Networks_Lab3/src/networks/lab3/UDPReceiver.java
+++ Networks_Lab3/src/networks/lab3/UDPReceiver.java
@@ -5,6 +5,8 @@ import java.net.*;
 
 public class UDPReceiver {
 	
+	String[] window;
+	
 	public static void main(String args[])throws Exception
 	{
 		DatagramSocket receiverSocket = new DatagramSocket(9876);
@@ -19,4 +21,41 @@ public class UDPReceiver {
 			
 		int port = rcvPkt.getPort();					
 	}
+	
+	/*public DatagramPacket ReceiveData(){
+		
+		byte[] receiveData = new byte[1024];
+		
+		return new DatagramPacket(receiveData, receiveData.length);
+	}*/
+	
+	//Initializing a new window 
+	public void newWindow(int windowSize){
+		window = new String[windowSize];
+		
+		for (int i = 0; i < windowSize; i++){
+			window[i] = String.valueOf(i);
+		}
+	}
+	
+	//Shifts window to the right	
+	public String[] shiftWindow(String[] window, int sequenceNumber) {
+    
+        for (int i = 0; i < window.length - 1; i++) {
+            window[i] = window[i + 1];
+        }
+        
+        return window;
+    }
+	
+	//Prints the window
+	public void printWindow(String[] window){
+		
+		System.out.print("[");
+		for (int i = 0; i < window.length; i++){
+			System.out.println(window[i] + ",");
+		}
+		System.out.print("]");
+	}
+	
 }
