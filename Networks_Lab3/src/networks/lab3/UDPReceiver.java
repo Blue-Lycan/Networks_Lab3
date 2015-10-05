1cc84a527f231109f8d96ee6baae34e90a7ec0c4
TJSchwartzt <TJSchwartzt@gmail.com>
2015-10-04 20:45:54 -0600

    Fixed merging errors from before.

--- Networks_Lab3/src/networks/lab3/UDPReceiver.java
+++ Networks_Lab3/src/networks/lab3/UDPReceiver.java
@@ -8,6 +8,7 @@ public class UDPReceiver {
 	private int windowSize;
 	private int maxSequenceNum;
 	private DatagramSocket receiverSocket;
+	private String[] window;
 	
 	UDPReceiver() throws Exception{
 		this.receiverSocket = new DatagramSocket(9876);
@@ -41,9 +42,47 @@ public class UDPReceiver {
 		else{
 			
 		}
+		
 	}
+	
+	public void DropPacket(){
+		//When to drop packet?
+	}
+	
+	public void newWindow(int windowSize){
+		
+		window = new String[windowSize];
+		
+		for (int i = 0; i < windowSize; i++){
+			window[i] = String.valueOf(i);
+		}
+	}
+	
+	//shifts window to the right
+	public String[] shiftWindow(String[] window, int sequenceNumber) {
+		    
+		for (int i = 0; i < window.length - 1; i++) {
+			window[i] = window[i + 1];
+			}
+		return window;
+	}
+	
+	public void printWindow(String[] window){
+		
+		System.out.print("[");
+		for (int i = 0; i < window.length; i++){
+			System.out.println(window[i] + ",");
+		}
+		System.out.print("]");
+	}
+	
 	public static void main(String args[])throws Exception
 	
 	{
 		UDPReceiver receiver = new UDPReceiver();
-}}
+		
+		while(true){
+			receiver.listenForData();
+		}
+	}
+}
